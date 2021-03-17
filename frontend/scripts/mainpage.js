df52b86e2f9960bbef8fa672164f5f197e94b13e
b62769na <nadine.abdelhalim@student.manchester.ac.uk>
2021-03-17 20:30:54 +0400

    Connect main page to DB

---
+++ frontend/scripts/mainpage.js
@@ -0,0 +1,56 @@
+const db = firebase.firestore();
+const carParkA_Data = db.collection("carParkData").doc("exampleCarPark(A)");
+
+const Afree_spaces = document.getElementById("free");
+const Ataken_spaces = document.getElementById("taken");
+const Atotal_spaces = document.getElementById("total");
+const Alocation = document.getElementById("location");
+const Apark = document.getElementById("park");
+
+// var user = localStorage.getItem("currentUser");
+// var user = "test@gmail.com";
+
+carParkA_Data.get().then((doc) => {
+    if (doc.exists) {
+        Ataken_spaces.innerText = doc.data().taken_spaces;
+        Atotal_spaces.innerText = doc.data().total_spaces;
+        Afree_spaces.innerText = (doc.data().total_spaces- doc.data().taken_spaces);
+        Alocation.innerText = "Car Park " + doc.data().name;
+        Apark.innerText = "Park at " + doc.data().name;
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+})
+.catch((error) => {
+		console.log("Error getting document:", error);
+});
+
+
+//car park proces constants
+const Ahalf = document.getElementById("half");
+const A1 = document.getElementById("one");
+const A2 = document.getElementById("two");
+const A3 = document.getElementById("three")
+const A4 = document.getElementById("four");
+const A5 = document.getElementById("five+");
+
+const carParkA_Prices = db.collection("prices").doc("c3mkCBrcCtxyfv6LFOBI");
+carParkA_Prices.get().then((doc) => {
+    if (doc.exists) {
+        console.log(doc.data().half);
+        Ahalf.innerText = doc.data().half;
+        console.log(doc.data().one);
+        A1.innerText = doc.data().one;
+        A2.innerText = doc.data().two;
+        A3.innerText = doc.data().three;
+        A4.innerText = doc.data().four;
+        A5.innerText = doc.data().five;
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+})
+.catch((error) => {
+		console.log("Error getting document:", error);
+});
