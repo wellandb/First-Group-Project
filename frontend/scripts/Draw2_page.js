4b148765ef55680fa52104cfd075150daae23ede
b62769na <nadine.abdelhalim@student.manchester.ac.uk>
2021-03-19 13:14:21 +0400

    Connect pages to DB

---
+++ frontend/scripts/Draw2_page.js
@@ -0,0 +1,24 @@
+const db = firebase.firestore();
+// var user = localStorage.getItem("currentUser");
+var user = "test@gmail.com";
+const userTicket = db.collection("userTicket").doc(user);
+
+const cost = document.getElementById("cost");
+const duration = document.getElementById("duration");
+const location2 = document.getElementById("location2");
+userTicket.get().then((doc) => {
+    if (doc.exists) {
+        let o = doc.data();
+        let key = Object.keys(o)[0];
+        cost.innerHTML = o[key].cost;
+        duration.innerHTML = o[key].duration;
+        location2.innerHTML = o[key].location;
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+})
+.catch((error) => {
+    console.log("Error getting document:", error);
+});
+    
